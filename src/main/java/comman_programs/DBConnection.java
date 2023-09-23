4c123cf39f643f712a54b50e1e282c85a6c4a707
Anurag8922 <anuragsy89@gmail.com>
2023-09-23 13:31:03 +0530

    Uploading Project

---
+++ src/main/java/comman_programs/DBConnection.java
@@ -0,0 +1,29 @@
+package comman_programs;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection
+{
+              private static Connection con;
+     
+     private DBConnection()
+     {
+    	 
+     }
+     
+     public static Connection getcon()
+     {
+    	 try
+    	 {
+    		Class.forName("oracle.jdbc.driver.OracleDriver");
+ 		    con=DriverManager.getConnection
+ 			      ("jdbc:oracle:thin:@localhost:1521:orcl","system","oracleanurag"); 
+    	 }
+    	 catch(Exception e)
+    	 {
+    		 e.printStackTrace();
+    	 }
+    	 return con;
+     }
+}
